f0499aa74ea40fe64685c2499ee53a841cee47c5
c2ksoria <carlosvsoria1982@gmail.com>
2022-03-15 15:34:34 -0300

    Se agregan funciones para el manejo de los permisos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +41,7 @@
             // 
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(12, 413);
+            this.button1.Location = new System.Drawing.Point(105, 397);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 49);
             this.button1.TabIndex = 0;
@@ -52,7 +53,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 358);
+            this.button2.Location = new System.Drawing.Point(11, 397);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 49);
             this.button2.TabIndex = 3;
@@ -60,12 +61,23 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Título";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.ClientSize = new System.Drawing.Size(938, 593);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -73,6 +85,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,6 +94,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,6 +21,8 @@ namespace testButton
         public DataTable newDataTable = new DataTable();
         public List<string> BotonesActuales = new List<string>();
         public BindingSource bind = new BindingSource();
+        List<(char, short)> aProcesar = new List<(char, short)> { ('N', 1), ('P', 2), ('E', 4), ('I', 8), ('C', 16), ('T', 32), ('Q', 64), ('B', 128), ('M', 256), ('A', 512) };
+        public string nprog = "";
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +33,29 @@ namespace testButton
             //LoadData();
         }
         #endregion
+
+        #region
+
+        public void LecturaPermisos(string NumeroDePrograma)
+        {
+            nprog = NumeroDePrograma;
+            var myJsonString = File.ReadAllText(@"C:\Users\Valdemar\Desktop\SFH\Pruebas\xxx\test\Nivel3\Recursos\Programas.json");
+
+            dynamic stuff = JsonConvert.DeserializeObject(myJsonString);
+            foreach (var s in stuff)
+            {
+                Console.WriteLine(s.nProg);
+                if (nprog == (string)s.nProg)
+                    {
+                    Console.WriteLine(s.Permisos);
+                    //Console.WriteLine(s.PersonalizacionBotones[0].NombreBotonNuevo);
+                    }
+            }
+        }
+        public void GetCompare()
+        {
+
+        }
 
         #region "Setup de botones en base a permisos"
         private void Setup(List<(string, bool)> aProcesar)
@@ -77,34 +103,43 @@ namespace testButton
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Hubo un error",ex.Message);
+                MessageBox.Show("Hubo un error", ex.Message);
             }
-            
+
         }
         #endregion
 
         #region "Lectura de Datos"
         void ImportarDatos(string nombrearchivo) //COMO PARAMETROS OBTENEMOS EL NOMBRE DEL ARCHIVO A IMPORTAR
         {
-            //UTILIZAMOS 12.0 DEPENDIENDO DE LA VERSION DEL EXCEL, EN CASO DE QUE LA VERSIÓN QUE TIENES ES INFERIOR AL DEL 2013, CAMBIAR A EXCEL 8.0 Y EN VEZ DE
-            //ACE.OLEDB.12.0 UTILIZAR LO SIGUIENTE (Jet.Oledb.4.0)
-            string conexion = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", nombrearchivo);
-
-            OleDbConnection conector = new OleDbConnection(conexion);
-
-            conector.Open();
-
-            //DEPENDIENDO DEL NOMBRE QUE TIENE LA PESTAÑA EN TU ARCHIVO EXCEL COLOCAR DENTRO DE LOS []
-            OleDbCommand consulta = new OleDbCommand("select * from [test$]", conector);
-
-            OleDbDataAdapter adaptador = new OleDbDataAdapter
-            {
-                SelectCommand = consulta
-            };
-
             DataTable ds = new DataTable();
-            adaptador.Fill(ds);
-            conector.Close();
+            try
+            {
+                //UTILIZAMOS 12.0 DEPENDIENDO DE LA VERSION DEL EXCEL, EN CASO DE QUE LA VERSIÓN QUE TIENES ES INFERIOR AL DEL 2013, CAMBIAR A EXCEL 8.0 Y EN VEZ DE
+                //ACE.OLEDB.12.0 UTILIZAR LO SIGUIENTE (Jet.Oledb.4.0)
+                string conexion = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", nombrearchivo);
+
+                OleDbConnection conector = new OleDbConnection(conexion);
+
+                conector.Open();
+
+                //DEPENDIENDO DEL NOMBRE QUE TIENE LA PESTAÑA EN TU ARCHIVO EXCEL COLOCAR DENTRO DE LOS []
+                OleDbCommand consulta = new OleDbCommand("select * from [test1$]", conector);
+
+                OleDbDataAdapter adaptador = new OleDbDataAdapter
+                {
+                    SelectCommand = consulta
+                };
+
+
+                adaptador.Fill(ds);
+                conector.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hubo un error", ex.Message);
+            }
+
             DataSet data = new DataSet("empleado");
             newDataTable = ds;
             bind.DataSource = ds;
@@ -113,7 +148,7 @@ namespace testButton
         {
             State.SetState("Buscar");
             StateChanged();
-            var path = @"C:\Users\Valdemar\Desktop\SFH\Pruebas\xxx\test\Recursos\test.xls";
+            var path = @"C:\Users\Valdemar\Desktop\SFH\Pruebas\xxx\test\Recursos\test1.xls";
             ImportarDatos(path);
             State.SetState("Mostrar");
             StateChanged();
@@ -149,69 +184,69 @@ namespace testButton
             button.Image = Image.FromFile(icono);
             button.ImageAlign = ContentAlignment.MiddleLeft;
             button.TextAlign = ContentAlignment.MiddleRight;
-            button.TextImageRelation= TextImageRelation.ImageBeforeText;
+            button.TextImageRelation = TextImageRelation.ImageBeforeText;
             Controls.Add(button);
         }
-        private void btnDynamicButton_Click(object sender, EventArgs e)
+        public virtual void btnDynamicButton_Click(object sender, EventArgs e)
         {
             Button btnDynamicButton = sender as Button;
 
-                switch (btnDynamicButton.Name)
-                {
-                    case "botonAbajo":
-                        {
+            switch (btnDynamicButton.Name)
+            {
+                case "botonAbajo":
+                    {
                         bind.MoveNext();
                         Console.WriteLine("moviendo abajo... Posición: ", bind.Position);
                         break;
-                        }
-                    case "botonArriba":
-                        {
+                    }
+                case "botonArriba":
+                    {
                         bind.MovePrevious();
                         Console.WriteLine("moviendo Arriba... Posición: ", bind.Position);
                         break;
-                        }
-                    case "botonFin":
-                        {
+                    }
+                case "botonFin":
+                    {
                         bind.MoveLast();
                         Console.WriteLine("moviendo FIN... Posición: ", bind.Position);
                         break;
-                        }
-                    case "botonInicio":
-                        {
+                    }
+                case "botonInicio":
+                    {
                         bind.MoveFirst();
                         Console.WriteLine("moviendo Inicio... Posición: ", bind.Position);
                         break;
-                        }
-                    case "botonImprimir":
-                        {
-                            Console.WriteLine("Imprimiendo...");
-                            break;
-                        }
-                    case "botonExportar":
-                        {
-                            Console.WriteLine("Exportando...");
-                            break;
-                        }
-                    case "botonSucursales":
-                        {
-                            Console.WriteLine("Sucursales...");
-                            break;
-                        }
-                    case "botonConfig":
-                        {
-                            Console.WriteLine("Configurar...");
-                            break;
-                        }
-                    case "botonUnidad":
-                        {
-                            Console.WriteLine("Unidad...");
-                            break;
-                        }
-
-
-                    default:
+                    }
+                case "botonImprimir":
+                    {
+                        Console.WriteLine("Imprimiendo...");
                         break;
-                }
+                    }
+                case "botonExportar":
+                    {
+                        Console.WriteLine("Exportando...");
+                        break;
+                    }
+                case "botonSucursales":
+                    {
+                        Console.WriteLine("Sucursales...");
+                        break;
+                    }
+                case "botonConfig":
+                    {
+                        Console.WriteLine("Configurar...");
+                        break;
+                    }
+                case "botonUnidad":
+                    {
+                        Console.WriteLine("Unidad...");
+                        break;
+                    }
+
+
+                default:
+                    break;
+            }
             if (btnDynamicButton.Name == "botonSalir")
             {
                 this.Close();
@@ -254,17 +289,33 @@ namespace testButton
         #region "Proceso de inicialización de botones del formulario"
         public void LoadData()
         {
+            ConfigVentana(SetupVentana);
             Setup(this.PermisosFuncionesNavegacion);
             Setup(this.PermisosFuncionesCustom);
             Setup(this.Basicas);
             State.SetState("Espera");
             StateChanged();
         }
+
+        #endregion
+
+        private void ConfigVentana(Tuple<int, int, bool, bool, string, string> setupVentana)
+        {
+            this.MinimizeBox = setupVentana.Item3;
+            this.MaximizeBox = setupVentana.Item4;
+            this.Text = setupVentana.Item5;
+            label1.Text = setupVentana.Item6;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Size = new System.Drawing.Size(setupVentana.Item1, setupVentana.Item2);
+
+        }
         #region "Variables globales a ser pasadas como parámetros"
         public List<(string, bool)> Basicas { get; set; }
         public List<(string, bool, string, string, string)> PermisosFuncionesCustom { get; set; }
         public List<(string, bool, string, int, int, int, int, string)> PosicionesBotones { get; set; }
         public List<(string, bool)> PermisosFuncionesNavegacion { get; set; }
+        public Tuple<int, int, bool, bool, string, string> SetupVentana { get; set; }
+
 
         #endregion
 
@@ -287,5 +338,125 @@ namespace testButton
             }
         }
         #endregion
+
+        #region Funciones de comparaciones de permisos
+        private short PermisoBinario(char[] lectura)
+        {
+            //Console.WriteLine("función procesar...");
+            short result = 0;
+
+            foreach (char item in lectura)
+            {
+                foreach ((char, short) tupla in aProcesar)
+                {
+                    //Console.WriteLine(tupla);
+                    if (item == tupla.Item1)
+                    {
+                        //Console.WriteLine("Hay una coincidencia: {0} -> {1}", tupla.Item1, tupla.Item2);
+                        result += tupla.Item2;
+                    }
+                }
+            }
+            Console.WriteLine("El resultado es: {0} ", result);
+            return result;
+        }
+
+
+        private void ComparaPermisos(string PermisoAcomparar, string PermisoReferencia)
+        {
+            char[] PaCompararArray = PermisoAcomparar.ToCharArray();
+            char[] PrefArray = PermisoReferencia.ToCharArray();
+
+            short dato1 = (short)PermisoBinario(PaCompararArray);
+            short dato2 = (short)PermisoBinario(PrefArray);
+
+            //TODO:Este proceso a continuación se podría hacer con un foreach
+            //lo dejo planteado para más adelante por cuestiones de avances del proyecto.
+
+            if ((dato1 & dato2 & aProcesar[9].Item2) == aProcesar[9].Item2)   //Letra A
+            {
+                //Console.WriteLine("TRUE: el resultado es A -> 512...procesar función. A Procesar: {0}, xx: {1}", aProcesar[9].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es A -> 512...procesar función. A Procesar: {0}, xx: {1}", aProcesar[9].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[8].Item2) == aProcesar[8].Item2)    //Letra M
+            {
+                //Console.WriteLine("TRUE: el resultado es M-> 256...procesar función. A Procesar: {0}, xx: {1}", aProcesar[8].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es M-> 256...procesar función. A Procesar: {0}, xx: {1}", aProcesar[8].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[7].Item2) == aProcesar[7].Item2)    //Letra B
+            {
+                //Console.WriteLine("TRUE: el resultado es B-> 128...procesar función. A Procesar: {0}, xx: {1}", aProcesar[7].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es B-> 128...procesar función. A Procesar: {0}, xx: {1}", aProcesar[7].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[6].Item2) == aProcesar[6].Item2)    //Letra Q
+            {
+                //Console.WriteLine("TRUE: el resultado es Q-> 64...procesar función. A Procesar: {0}, xx: {1}", aProcesar[6].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es Q-> 64...procesar función. A Procesar: {0}, xx: {1}", aProcesar[6].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[5].Item2) == aProcesar[5].Item2)    //Letra T
+            {
+                //Console.WriteLine("TRUE: el resultado es T-> 32...procesar función. A Procesar: {0}, xx: {1}", aProcesar[5].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es T-> 32...procesar función. A Procesar: {0}, xx: {1}", aProcesar[5].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[4].Item2) == aProcesar[4].Item2)    //Letra C
+            {
+                //Console.WriteLine("TRUE: el resultado es C-> 16...procesar función. A Procesar: {0}, xx: {1}", aProcesar[4].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es C-> 16...procesar función. A Procesar: {0}, xx: {1}", aProcesar[4].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[3].Item2) == aProcesar[3].Item2)    //Letra I
+            {
+                //Console.WriteLine("TRUE: el resultado es I-> 8...procesar función. A Procesar: {0}, xx: {1}", aProcesar[3].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es I-> 8...procesar función. A Procesar: {0}, xx: {1}", aProcesar[3].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[2].Item2) == aProcesar[2].Item2)    //Letra E
+            {
+                //Console.WriteLine("TRUE: el resultado es E-> 4...procesar función. A Procesar: {0}, xx: {1}", aProcesar[2].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es E-> 4 ...procesar función. A Procesar: {0}, xx: {1}", aProcesar[2].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[1].Item2) == aProcesar[1].Item2)    //Letra P
+            {
+                //Console.WriteLine("TRUE: el resultado es P-> 2 ...procesar función. A Procesar: {0}, xx: {1}", aProcesar[1].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es P-> 2 ...procesar función. A Procesar: {0}, xx: {1}", aProcesar[1].Item2, xx);
+            }
+            if ((dato1 & dato2 & aProcesar[0].Item2) == aProcesar[0].Item2)    //Letra N
+            {
+                //Console.WriteLine("TRUE: el resultado es N-> 1 ...procesar función. A Procesar: {0}, xx: {1}", aProcesar[9].Item2, xx);
+            }
+            else
+            {
+                //Console.WriteLine("FALSE: el resultado es N-> 1 ...procesar función. A Procesar: {0}, xx: {1}", aProcesar[0].Item2, xx);
+            }
+
+            Console.WriteLine("-----------FIN----------");
+        }
+        #endregion
     }
 }
+        
